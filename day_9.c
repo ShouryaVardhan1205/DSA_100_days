70f3931f1f761e2cf1909050ac5251d5df49900b
ShouryaVardhan1205 <shouryavardhan1205@gmail.com>
2026-02-09 17:43:40 +0530

    Add files via upload

---
+++ day_9.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[1000];
+
+    printf("Enter code name: ");
+    scanf("%s", str);
+
+    int len = strlen(str);
+
+    printf("Mirror format: ");
+    for(int i = len - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
